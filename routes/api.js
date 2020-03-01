8a9997bfb85132058b55da633c511cb59e8dd0f5
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2020-02-29 17:28:45 -0800

    added code to routes folder

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,53 @@
-const router = require("express").Router();
-const db = require("../models")
+const express = require("express");
+const router = express.Router();
+const Workout = require("../models/workout");
 
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then(data => {
+      res.json(data);
+      console.log(data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then(data => {
+      res.json(data);
+      console.log(data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+router.put(
+  "/api/workouts/:id",
+  ({ body, params },
+  (req, res) => {
+    const getId = req.params.id;
+    const getBody = req.body;
+    Workout.findByIdAndUpdate({ id: getId }, { $push: { exercise: getBody } })
+      .then(data => {
+        res.json(data);
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  })
+);
+
+router.get("/api/workout/range", (req, res) => {
+    Workout.find()
+        .then(data => {
+            res.json(data);
+            console.log(data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
@@ -16,7 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker"
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js/index.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
